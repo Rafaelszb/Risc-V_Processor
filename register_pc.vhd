791083f286207638edba2f81e8c01be98b8b45dd
Rafaelszb <rafaelsouzas153@gmail.com>
2025-06-18 16:24:37 -0300

    Salvo

--- register_pc.vhd
+++ register_pc.vhd
@@ -4,14 +4,14 @@ use IEEE.std_logic_1164.all;
 entity registrador is
 port (
       i_CLK : in std_logic;
-      i_DATA : in std_logic_vector(7 downto 0);
-      o_DATA : out std_logic_vector(7 downto 0)
+      i_DATA : in std_logic_vector(4 downto 0);
+      o_DATA : out std_logic_vector(4 downto 0)
 );
 
 end registrador;
 
 architecture arch of registrador is
-signal w_DATA : std_logic_vector(7 downto 0) := "00000000";
+signal w_DATA : std_logic_vector(4 downto 0) := "00000";
 
 begin
 process(i_CLK)
